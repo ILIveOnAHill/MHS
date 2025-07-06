5fd4b67e6c933f351f4856ff5d8843147081d84d
Aljaž Hribernik <hribernik.aljaz@gmail.com>
2025-07-06 13:43:42 +0200

    first commit

---
+++ js/index3.js
@@ -0,0 +1,84 @@
+(function ($) {
+
+    /**
+     * Copyright 2012, Digital Fusion
+     * Licensed under the MIT license.
+     * http://teamdf.com/jquery-plugins/license/
+     *
+     * @author Sam Sehnert
+     * @desc A small plugin that checks whether elements are within
+     *     the user visible viewport of a web browser.
+     *     only accounts for vertical position, not horizontal.
+     */
+
+    $.fn.visible = function (partial) {
+
+        var $t = $(this),
+                $w = $(window),
+                viewTop = $w.scrollTop(),
+                viewBottom = viewTop + $w.height(),
+                _top = $t.offset().top,
+                _bottom = _top + $t.height(),
+                compareTop = partial === true ? _bottom : _top,
+                compareBottom = partial === true ? _top : _bottom;
+
+        return ((compareBottom <= viewBottom) && (compareTop >= viewTop));
+
+    };
+
+})(jQuery);
+
+var win = $(window);
+var allMods = $(".module");
+
+allMods.each(function (i, el) {
+    var el = $(el);
+    if (el.visible(true)) {
+        el.addClass("already-visible");
+    }
+});
+
+win.scroll(function (event) {
+    allMods.each(function (i, el) {
+        var el = $(el);
+        if (el.visible(true)) {
+            el.addClass("come-in");
+        }
+    });
+});
+
+
+
+
+$(window).scroll(function(){
+  var wh = $(window).height()-100;
+	if($(window).scrollTop() > $('.header').offset().top-wh){
+  	$('.header').addClass('play');}
+             
+});
+
+
+
+$(window).scroll(function(){
+  var wh = $(window).height()-100;
+	if($(window).scrollTop() > $('.AboutText').offset().top-wh){
+  	$('.AboutText').addClass('play');}
+  
+});
+
+
+
+        
+document.getElementById("container").addEventListener("wheel", myFunction);
+document.getElementById("contents").addEventListener("click", myFunction);
+
+function myFunction() {
+    document.location.href = "#Produkti";
+}
+
+
+
+
+function myFunction2(x) {
+  x.classList.toggle("change");
+}
